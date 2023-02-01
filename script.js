93e47f3fed3391c69ceec84dae2e73bc91dbc7e3
+ <+>
2023-02-01 16:58:45 -0500

    adds JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "First personal Creation";
 
 document.querySelector("body").appendChild(h2);
+
